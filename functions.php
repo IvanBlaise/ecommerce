e2ba1c59ab197cfc9cdee8b0bf884fe181e9b8f6
ivan carlos <ivancarlos50@gmail.com>
2019-05-13 12:26:48 -0300

    Site - Produtos

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php 
+
+function formatPrice(float $vlprice)
+{
+
+	return number_format($vlprice, 2, ",", ".");
+
+
+}
+
+
+
+
+ ?>
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -18,6 +18,23 @@ class Product extends Model {
 		return $sql->select("select * from tb_products ORDER BY desproduct");
 	}
 
+	public static function checkList($list)
+	{
+
+		foreach ($list as &$row) {
+			
+			$p = new Product();
+
+			$p->setData($row);
+
+			$row = $p->getvalues();
+		}
+
+		return $list;
+
+	}
+
+	
 	public function save()
 	{
 
